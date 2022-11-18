e850700e0706b99bfffc51635c6d04851600d429
Da-Vilen <54321anuragyadav@gmail.com>
2022-11-18 19:55:39 +0530

    First push for dark theme toggle

---
+++ index.js
@@ -0,0 +1,19 @@
+var checkBox=document.querySelector('input[name=theme]');
+
+checkBox.addEventListener('change',function(){
+    if(this.checked) {
+        trans();
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        trans();
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+
+})
+
+let trans=() => {
+    document.documentElement.classList.add('transition');
+    window.setTimeout(() => {
+        document.documentElement.classList.remove('transition')
+    }, 1000);
+}
